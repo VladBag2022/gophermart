75bb633b299d4bf96cdb776262f82a55b47467d0
Vladislav Bag <evgeng49@yandex.ru>
2022-08-01 14:42:33 +0300

    Add tests for register handler

--- internal/server/handlers_test.go
+++ internal/server/handlers_test.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/stretchr/testify/assert"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -33,49 +34,87 @@ func makeTestRequest(t *testing.T, ts *httptest.Server, method, path string, bod
 	return response, string(responseBody)
 }
 
-func getTestServer() *httptest.Server {
+func getTestServer(logins []string) *httptest.Server {
 	return &httptest.Server{}
 }
 
-func TestServer_shorten(t *testing.T) {
+func TestServer_register(t *testing.T) {
 	type want struct {
-		contentType string
 		statusCode  int
-		response    bool
 	}
 	tests := []struct {
-		name    string
-		content string
-		want    want
+		name    	string
+		logins		[]string
+		contentType string
+		content 	string
+		want    	want
 	}{
 		{
-			name:    "positive test",
-			content: "https://example.com",
+			name:    		"positive test with some logins",
+			logins: 		[]string{"a", "b"},
+			contentType: 	"application/json",
+			content: 		"{\"login\": \"c\",\"password\": \"123\"}",
 			want: want{
-				contentType: "text/plain; charset=utf-8",
-				statusCode:  201,
-				response:    true,
+				statusCode:  200,
 			},
 		},
 		{
-			name:    "negative test",
-			content: "",
+			name:    		"positive test without any logins",
+			logins: 		[]string{},
+			contentType: 	"application/json",
+			content: 		"{\"login\": \"c\",\"password\": \"123\"}",
 			want: want{
-				contentType: "text/plain; charset=utf-8",
+				statusCode:  200,
+			},
+		},
+		{
+			name:    		"negative test with empty login",
+			logins: 		[]string{},
+			contentType: 	"application/json",
+			content: 		"{\"login\": \"\",\"password\": \"123\"}",
+			want: want{
 				statusCode:  400,
-				response:    true,
+			},
+		},
+		{
+			name:    		"negative test with empty password",
+			logins: 		[]string{},
+			contentType: 	"application/json",
+			content: 		"{\"login\": \"c\",\"password\": \"\"}",
+			want: want{
+				statusCode:  400,
+			},
+		},
+		{
+			name:    		"negative test with empty login and password",
+			logins: 		[]string{},
+			contentType: 	"application/json",
+			content: 		"{\"login\": \"\",\"password\": \"\"}",
+			want: want{
+				statusCode:  400,
+			},
+		},
+		{
+			name:    		"negative test with duplicate login",
+			logins: 		[]string{"a", "b"},
+			contentType: 	"application/json",
+			content: 		"{\"login\": \"a\",\"password\": \"123\"}",
+			want: want{
+				statusCode:  409,
 			},
 		},
 	}
-
-	ts := getTestServer()
-	defer ts.Close()
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			response, _ := makeTestRequest(t, ts, http.MethodPost, "/", strings.NewReader(tt.content))
+			ts := getTestServer(tt.logins)
+			defer ts.Close()
+
+			response, _ := makeTestRequest(t, ts, http.MethodPost, "/api/user/register",
+				strings.NewReader(tt.content))
 			err := response.Body.Close()
 			require.NoError(t, err)
+
+			assert.Equal(t, tt.want.statusCode, response.StatusCode)
 		})
 	}
 }
