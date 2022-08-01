1b0e01ba21be2350dfd410b081a59afe6e918ca4
Vladislav Bag <evgeng49@yandex.ru>
2022-08-01 15:46:55 +0300

    Add tests for list orders handler

--- internal/server/handlers_test.go
+++ internal/server/handlers_test.go
@@ -239,7 +239,7 @@ func TestServer_login(t *testing.T) {
 	}
 }
 
-func TestServer_orders(t *testing.T) {
+func TestServer_new_order(t *testing.T) {
 	type want struct {
 		statusCode  int
 	}
@@ -355,6 +355,76 @@ func TestServer_orders(t *testing.T) {
 			require.NoError(t, err)
 
 			assert.Equal(t, tt.want.statusCode, response.StatusCode)
+		})
+	}
+}
+
+
+func TestServer_list_orders(t *testing.T) {
+	type want struct {
+		statusCode  int
+		contentType string
+		content 	bool
+	}
+
+	tests := []struct {
+		name    	string
+		userOrders 	map[string]bool
+		user 		string
+		want    	want
+	}{
+		{
+			name:    		"positive test - data is presented",
+			userOrders:		map[string]bool{
+				"a": true,
+				"b": false,
+			},
+			user:			"a",
+			want: want{
+				statusCode:		200,
+				contentType: 	"application/json",
+				content: 	 	true,
+			},
+		},
+		{
+			name:    		"positive test - no data",
+			userOrders:		map[string]bool{
+				"a": true,
+				"b": false,
+			},
+			user:			"b",
+			want: want{
+				statusCode:		204,
+				contentType: 	"",
+				content: 	 	false,
+			},
+		},
+		{
+			name:    		"negative test - unauthorized",
+			userOrders:		map[string]bool{
+				"a": true,
+				"b": false,
+			},
+			user:			"c",
+			want: want{
+				statusCode:		401,
+				contentType: 	"",
+				content: 	 	false,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := &httptest.Server{}
+			defer ts.Close()
+
+			response, content := makeTestRequest(t, ts, http.MethodGet, "/api/user/orders", nil)
+			err := response.Body.Close()
+			require.NoError(t, err)
+
+			assert.Equal(t, tt.want.statusCode, response.StatusCode)
+			assert.Equal(t, tt.want.contentType, response.Header.Get("Content-Type"))
+			assert.Equal(t, tt.want.content, len(content) > 0)
 		})
 	}
 }
