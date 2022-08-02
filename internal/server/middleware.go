5ab9734a03829fc5ea3c124f9a1d344bccb8887b
Vladislav Bag <evgeng49@yandex.ru>
2022-08-02 10:58:33 +0300

    Remove comments

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -6,17 +6,16 @@ import (
 )
 
 func DecompressGZIP(next http.Handler) http.Handler {
-	// приводим возвращаемую функцию к типу функций HandlerFunc
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get(`Content-Encoding`) == `gzip` { //	если входящий пакет сжат GZIP
-			gz, err := gzip.NewReader(r.Body) //	изготавливаем reader-декомпрессор GZIP
+		if r.Header.Get(`Content-Encoding`) == `gzip` {
+			gz, err := gzip.NewReader(r.Body)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusBadRequest)
 				return
 			}
-			r.Body = gz //	подменяем стандартный reader из Request на декомпрессор GZIP
+			r.Body = gz
 			defer gz.Close()
 		}
-		next.ServeHTTP(w, r) // передаём управление следующему обработчику
+		next.ServeHTTP(w, r)
 	})
 }
