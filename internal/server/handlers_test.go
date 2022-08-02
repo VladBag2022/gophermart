36fb361848c702e0ecac35032a81db2b8600f71f
Vladislav Bag <evgeng49@yandex.ru>
2022-08-02 09:57:11 +0300

    import

--- internal/server/handlers_test.go
+++ internal/server/handlers_test.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"github.com/stretchr/testify/assert"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -9,6 +8,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
